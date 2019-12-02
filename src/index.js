51ef6b31fe6e056c302218f9b20ac1454530fa29
SBerdnikau <30774859+SBerdnikau@users.noreply.github.com>
2019-12-02 19:01:42 +0300

    breakpoint 2 update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './scss/index.scss';
 import './js/hamburger';
 import './js/menu';
-import './js/langMenu';
 import './js/owl.carousel.min';
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,21 +1,76 @@
 
-document.querySelector('.menu__list').onmouseover= function(event) {
-    event.preventDefault();
-    var target = event.target; // где был клик?
-    if (target.className == 'menu__element') {
-        var s = target.getElementsByClassName('submenu');
-        closeMenu();
-        s[0].style.display = 'block';
-    }
-};
+// document.querySelector('.menu__list').onmouseover= function(event) {
+//     event.preventDefault();
+//     var target = event.target; // где был клик?
+//     if (target.className == 'menu__element') {
+//         var s = target.getElementsByClassName('submenu');
+//         closeMenu();
+//         s[0].style.display = 'block';
+//     }
+// };
+//
+// function closeMenu() {
+//     var menu = document.querySelector('.menu__list');
+//     var subm = document.getElementsByClassName('submenu');
+//     for (var i=0; i<subm.length; i++) {
+//         subm[i].style.display="none";
+//     }
+// }
 
-function closeMenu() {
-    var menu = document.querySelector('.menu__list');
-    var subm = document.getElementsByClassName('submenu');
-    for (var i=0; i<subm.length; i++) {
-        subm[i].style.display="none";
+
+window.onload = function () {
+    var subMenu,
+        subMenuStyle,
+        menuStyle,
+        opener,
+        el = document.getElementsByClassName('menu-item'),
+        winWidth = document.documentElement.clientWidth;
+    for(var i=0; i<el.length; i++){
+        if(el[i].children.length > 1) {
+            opener = document.createElement('span');
+            opener.innerHTML = ">>";
+            el[i].insertBefore(opener, el[i].children[1]);
+            if(winWidth>=768){
+                el[i].addEventListener("mouseenter",showSub,false);
+                el[i].addEventListener("mouseleave",hideSub,false);
+            } else {
+                el[i].children[1].addEventListener("click",subOpen,false);
+            }
+        }
+    }
+    function showSub() {
+        subMenu = this.children[2];
+        subMenu.style.height = "auto";
+        subMenu.style.overflow = "visible";
+        subMenu.style.opacity = "1";
+    }
+    function hideSub() {
+        subMenu = this.children[2];
+        subMenu.style.height = "0px";
+        subMenu.style.overflow = "hidden";
+        subMenu.style.opacity = "0";
+    }
+    function subOpen() {
+        subMenu = this.nextElementSibling;
+        subMenuStyle = getComputedStyle(subMenu);
+        if(subMenuStyle.height == "0px"){
+            subMenu.style.height = "auto";
+            subMenu.style.overflow = "visible";
+            subMenu.style.opacity = "1";
+        } else {
+            subMenu.style.height = "0px";
+            subMenu.style.overflow = "hidden";
+            subMenu.style.opacity = "0";
+        }
+    }
+    menuStyle = getComputedStyle(menu);
+    openMenu.onclick = function () {
+        if(menuStyle.display == "none"){
+            menu.classList.add("active");
+            this.innerHTML = "Закрыть меню";
+        } else {
+            menu.classList.remove("active");
+            this.innerHTML = "Открыть меню";
+        }
     }
 }
-
-
-
