f8c4b0fdd431c8fa70912a4b5ec08e711f35185d
SBerdnikau <30774859+SBerdnikau@users.noreply.github.com>
2019-12-10 21:48:21 +0300

    breakpoint 8 update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './scss/index.scss';
 import './js/hamburger';
 import './js/menu';
+import './js/langMenu'
 import './js/owl.carousel.min';
--- src/js/langMenu.js
+++ src/js/langMenu.js
@@ -1,21 +1,25 @@
 
-document.querySelector('.menu__language').onmouseover= function(event) {
-    event.preventDefault();
-    if (target.className == 'header__item') {
-        target.getElementsByClassName('submenu').style.display = 'block';
-    }
-    //var target = event.target; // где был клик?
-    // if (target.className == 'language') {
-    //     var s = target.getElementsByClassName('submenu');
-    //     closeMenu();
-    //     s[0].style.display = 'block';
-    // }
-};
+const el = document.getElementById('language');
+const lang = document.querySelectorAll('.language__item');
 
-// function closeMenu() {
-//     var menu = document.querySelector('.menu__language');
-//     var subm = document.getElementsByClassName('submenu');
-//     for (var i=0; i<subm.length; i++) {
-//         subm[i].style.display="none";
-//     }
-// }
+
+
+if(el){
+    el.addEventListener('click', function (e) {
+        e.target.closest('.header__item').childNodes[3].nextElementSibling.classList.toggle('language__list--active');
+
+    });
+
+}
+
+
+lang.addEventListener('click',function (e) {
+   let lg = e.target.closest('.language__list').childNodes[2].nextSibling.textContent;
+   console.log(lg);
+});
+
+
+
+
+
+
